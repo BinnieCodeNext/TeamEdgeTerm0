7e116ced235b5ccbf2dbb462a28de8b459e68ec8
TBVoNguyen22 <67927979+TBVoNguyen22@users.noreply.github.com>
2020-07-26 19:09:12 -0700

    Update OrderBotProject.js
    Order Bot project, hmmm looked like Coach Alex's but ehh there were some changes. Used my resources lolll

--- OrderBotProject.js
+++ OrderBotProject.js
@@ -26,6 +26,13 @@ After this program finishes running, it should output a specific total for each 
   -------------------------------------------- */
 
 const READLINE = require("readline-sync");
+console.log("Haiii everyonee, welcome to the shoppp!!");
+let usr1Name = READLINE.question("User 1 please enter your name: ");
+console.log("User 1's name is " + usr1Name);
+let usr2Name = READLINE.question("User 2 please enter your name: ");
+console.log("User 2's name is " + usr2Name);
+let usr3Name = READLINE.question("User 3 please enter your name: ");
+console.log("User 3's name is " + usr3Name);
 
 /* -------------------------------------------- 
 
@@ -40,15 +47,26 @@ Remember! Functions are meant to be reusable, so write a function that will work
 
 -------------------------------------------- */
 
+let usr1Food;
+let usr1Drink;
+let usr2Food;
+let usr2Drink;
+let usr3Food;
+let usr3Drink;
 
+function takeUsrOrder(user, item){
+	cost = parseInt(READLINE.question(`Enter the price of ${user}'s ${item}: `));
+	return cost;
+}
 
+usr1Food = takeUsrOrder(usr1Name, `meal`);
+usr1Drink = takeUsrOrder(usr1Name, `drink`);
+usr2Food = takeUsrOrder(usr2Name, `meal`);
+usr2Drink = takeUsrOrder(usr2Name, `drink`);
+usr3Food = takeUsrOrder(usr3Name, `meal`);
+usr3Drink = takeUsrOrder(usr3Name, `drink`);
 
-
-
-
-
-
-
+console.log(usr1Food, usr1Drink, usr2Food, usr2Drink, usr3Food, usr3Drink);
 
 
 /* -------------------------------------------- 
@@ -66,14 +84,34 @@ Remember! Functions are meant to be reusable, so write a function that will work
 -------------------------------------------- */
 
 
+let usr1Total;
+let usr2Total;
+let usr3Total;
 
+function calculateCosts(food, drink){
+	usrTip = READLINE.question(`How much do you want to tip?  5%?  10%?  20%? `);
+	meal = food + drink;
+	tax = meal * 0.0925;
+	totalwTax = meal + tax;
+	let tip = 0
+	if(usrTip == `5%`){
+		tip = totalwTax * .05;	
+	}
+	else if(usrTip == `10%`){
+		tip = totalwTax * .1;	
+	}
+	else if(usrTip == `20%`){
+		tip = totalwTax * .2;	
+	}
+	total = totalwTax + tip;
+	return total;
+}
 
+usr1Total = calculateCosts(usr1Food, usr1Drink);
+usr2Total = calculateCosts(usr2Food, usr2Drink);
+usr3Total = calculateCosts(usr3Food, usr3Drink);
 
-
-
-
-
-
+console.log(usr1Total, usr2Total, usr3Total);
 
 
 /* -------------------------------------------- 
@@ -92,12 +130,16 @@ Remember! Functions are meant to be reusable, so write a function that will work
 -------------------------------------------- */
 
 
+function usrReceipt(usr, food, drink, total) {
+	console.log(`${usr}: Food: ${food}
+		                   Drink: ${drink}
+		                + tax & tip
+		                  Total: ${total}`);
+}
 
-
-
-
-
-
+console.log("User 1's receipt is " + usrReceipt(usr1Name, usr1Food, usr1Drink, usr1Total));
+console.log("User 2's receipt is " + usrReceipt(usr2Name, usr2Food, usr2Drink, usr2Total));
+console.log("User 3's receipt is " + usrReceipt(usr3Name, usr3Food, usr3Drink, usr3Total));
 
 
 /* -------------------------------------------- 
